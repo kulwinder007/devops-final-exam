dffaa577bb367a68953dc37b548f995977f4d86b
kulwinder007 <kulwindersingh915.ca@gmail.com>
2025-04-09 19:10:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Wild Rydes!'
+    return 'Hello, Kulwinder - App running on ECS!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
